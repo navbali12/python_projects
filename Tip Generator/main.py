8bab9ea916ab84e3fe7dd004dad4a543e2c519dc
navbali12 <n.bali706@mybvc.ca>
2023-08-17 19:53:27 -0600

    commit some additional changes in logic

--- Tip Generator/main.py
+++ Tip Generator/main.py
@@ -2,8 +2,8 @@
 
 print('HYE! I am a tip calculator!')
 
-b=input("What was the total bill?")
-t=input("How much tip would you like to give? 10, 12, or 15?")
+b=input("What was the total bill? \n$ ")
+t=input("How much tip would you like to give? 10, 12, or 15? \n " )
 p=input("How many people to split the bill?")
 
 bill=float(b)
@@ -15,3 +15,4 @@ total=round(((bill*(tip/100)+bill)/people),2)
 
 
 print(f"Each person should pay:${total}")
+print("thank you")
