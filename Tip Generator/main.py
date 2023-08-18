f0653838e2c046d0f36fbc502c743fa62f1c8fe6
navbali12 <n.bali706@mybvc.ca>
2023-08-17 18:39:52 -0600

    commit message

--- Tip Generator/main.py
+++ Tip Generator/main.py
@@ -1,1 +1,16 @@
+# welcome message
 
+print('Welcome to the tip calculator!')
+
+b=input("What was the total bill?")
+t=input("How much tip would you like to give? 10, 12, or 15?")
+p=input("How many people to split the bill?")
+
+bill=int(b)
+tip=int(t)
+people=int(p)
+
+#logic for finding tip
+total=bill*(tip/100)+bill
+
+print(f"Each person should pay:${total}")
